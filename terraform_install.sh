fe60d554c8c8b4e879353fcce3db1261dc53de54
Jacob Wyke <jacob@jacobwyke.com>
2018-08-20 15:41:51 +0100

    Getting terraform installed

---
+++ terraform_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /tmp
+wget https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_linux_amd64.zip
+unzip terraform_0.11.1_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
