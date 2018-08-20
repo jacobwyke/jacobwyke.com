4f822a02cb8be2d27769dcb87c1c618f2097fb36
Jacob Wyke <jacob@jacobwyke.com>
2018-08-20 16:05:14 +0100

    Trying different method to install terraform

--- terraform_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo /usr/bin/apt install -y unzip
-cd /tmp
-wget https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_linux_amd64.zip
-unzip terraform_0.11.1_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
-
-return 0
