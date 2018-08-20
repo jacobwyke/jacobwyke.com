afb79ea2e00ef7d70508c9af6e64132ba76987c8
Jacob Wyke <jacob@jacobwyke.com>
2018-08-20 15:52:53 +0100

    Ensuring we have the privs to install `unzip`

--- terraform_install.sh
+++ terraform_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo /usr/bin/apt install unzip
 cd /tmp
 wget https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_linux_amd64.zip
 unzip terraform_0.11.1_linux_amd64.zip
