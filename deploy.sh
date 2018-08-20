ad3d9bd6190f8799aa9450441e765b4c22a9edb4
Jacob Wyke <jacob@jacobwyke.com>
2018-08-20 18:05:33 +0100

    Removing the unsupported params

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #/bin/bash
 
-s3cmd --config=.s3cfg --help
+s3cmd sync build/assets/ s3://assets-jacobwyke-com --no-preserve --add-header='Cache-Control:public, max-age=31536000' --cf-invalidate --config=.s3cfg
 
-s3cmd sync build/assets/ s3://assets-jacobwyke-com --no-preserve --add-header='Cache-Control:public, max-age=31536000' --cf-invalidate --config=.s3cfg --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY
-
-s3cmd sync build/www/ s3://jacobwyke-com --delete-removed --no-preserve --add-header='Cache-Control:public, max-age=3600' --cf-invalidate --config=.s3cfg --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY
+s3cmd sync build/www/ s3://jacobwyke-com --delete-removed --no-preserve --add-header='Cache-Control:public, max-age=3600' --cf-invalidate --config=.s3cfg
