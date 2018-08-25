8c6c99c201339a89c82f8ab42981af792f8fab5a
Jacob Wyke <jacob@jacobwyke.com>
2018-08-25 02:42:22 +0100

    Trying the deploy without the cf-invalidate

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-s3cmd sync build/assets/ s3://assets-jacobwyke-com --no-preserve --add-header='Cache-Control:public, max-age=31536000' --cf-invalidate --config=.s3cfg
+s3cmd sync build/assets/ s3://assets-jacobwyke-com --no-preserve --add-header='Cache-Control:public, max-age=31536000' --config=.s3cfg
 
-s3cmd sync build/www/ s3://jacobwyke-com --delete-removed --no-preserve --add-header='Cache-Control:public, max-age=3600' --cf-invalidate --config=.s3cfg
+s3cmd sync build/www/ s3://jacobwyke-com --delete-removed --no-preserve --add-header='Cache-Control:public, max-age=3600' --config=.s3cfg
